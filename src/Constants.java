3c50ef7acd9d2830478cb240c6d441c706930f3b
AYRAT1 <ayrat1@mail.ru>
2019-09-15 10:21:05 +0300

    task complete

---
+++ src/Constants.java
@@ -0,0 +1,3 @@
+public class Constants {
+    public static final int ONE_HUNDRED_KILOMETER = 100;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            int distance;
+            System.out.println("Enter your fuel consumption in liters per 100 km");
+            showAlertMessage();
+            int fuelConsumption = scanner.nextInt();
+
+            System.out.println("Enter your fuel remaining in liters");
+            showAlertMessage();
+            int fuelRemaining = scanner.nextInt();
+
+            distance = fuelRemaining * Constants.ONE_HUNDRED_KILOMETER / fuelConsumption;
+            System.out.println("You can drive more about " + distance + " km");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Unsupported values, restart program and try again");
+        }
+    }
+
+    private static void showAlertMessage() {
+        System.out.println("INTEGER VALUE ONLY");
     }
 }
