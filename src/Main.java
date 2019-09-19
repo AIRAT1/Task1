d8079bb174874b776a3fe5902cbffa8e15023fca
AIRAT1 <ayrat1@mail.ru>
2019-09-19 15:25:52 +0300

    all comments corrected

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,24 @@ public class Main {
     public static void main(String[] args) {
 
         try (Scanner scanner = new Scanner(System.in)) {
-            int distance;
             System.out.println("Enter your fuel consumption in liters per 100 km");
             showAlertMessage();
-            int fuelConsumption = scanner.nextInt();
+            double fuelConsumption = scanner.nextDouble();
 
             System.out.println("Enter your fuel remaining in liters");
             showAlertMessage();
-            int fuelRemaining = scanner.nextInt();
+            double fuelRemaining = scanner.nextDouble();
 
-            distance = fuelRemaining * Constants.ONE_HUNDRED_KILOMETER / fuelConsumption;
+            int distance = (int) (fuelRemaining * Constants.ONE_HUNDRED_KILOMETER / fuelConsumption);
             System.out.println("You can drive more about " + distance + " km");
         } catch (Exception e) {
-            e.printStackTrace();
             System.out.println("Unsupported values, restart program and try again");
+            e.printStackTrace();
         }
     }
 
     private static void showAlertMessage() {
-        System.out.println("INTEGER VALUE ONLY");
+        System.out.println("INTEGER OR DOUBLE VALUE ONLY");
+        System.out.println("DOUBLE VALUES SEPARATE ONLY BY COMMA");
     }
 }
